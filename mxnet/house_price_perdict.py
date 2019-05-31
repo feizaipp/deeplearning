c1f24360b57a81dc905cc3f029648e490f72a9c9
zpehome <zpehome@yeah.net>
2019-05-31 19:32:57 +0800

    hource prices perdict

---
+++ mxnet/house_price_perdict.py
@@ -0,0 +1,27 @@
+import d2lzh as d2l
+from mxnet import autograd, gluon, init, nd
+from mxnet.gluon import data as gdata, loss as gloss, nn
+import numpy as np
+import pandas as pd
+
+train_data = pd.read_csv('../data/hourse_prices/train.csv')
+test_data = pd.read_csv('../data/hourse_prices/test.csv')
+
+print(train_data.iloc[0:4, [0, 1, 2, 3, -3, -2, -1]])
+
+all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
+
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+all_features[numeric_features] = all_features[numeric_features].apply(lambda x : (x-x.mean()) / (x.std()))
+# 标准化后，每个特征的均值变为0，所以可以直接用0来替换缺失值
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
+
+# dummy_na=True将缺失值也当作合法的特征值并为其创建指示特征
+all_features = pd.get_dummies(all_features, dummy_na=True)
+all_features.shape
+
+n_train = train_data.shape[0]
+train_features = nd.array(all_features[:n_train].values)
+test_features = nd.array(all_features[n_train:].values)
+train_labels = nd.array(train_data.SalePrice.values).reshape((-1, 1))
+
